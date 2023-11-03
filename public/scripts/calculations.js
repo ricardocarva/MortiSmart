f6983582a3eb79d5e6e99ae7024ecd3545f9293f
thomas-martin-uf <thomas.martin@ufl.edu>
2023-11-03 18:40:31 -0400

    adding more unit tests for calculations

--- public/scripts/calculations.js
+++ public/scripts/calculations.js
@@ -16,52 +16,69 @@ export const getMonthlyPayments = (loan, interest, term) => {
 };
 
 export const getTotalInterest = (fixedMonthly, loan, interest, term) => {
-    const months = term * 12;
-    const mRate = interest / (12 * 100);
-    let paymentTowards = 0.0,
-        totalInterest = 0.0;
+    if (
+        isNumber(fixedMonthly) &&
+        isNumber(loan) &&
+        isNumber(interest) &&
+        isNumber(term)
+    ) {
+        const months = term * 12;
+        const mRate = interest / (12 * 100);
+        let paymentTowards = 0.0,
+            totalInterest = 0.0;
 
-    for (let i = 0; i <= months; i++) {
-        totalInterest += loan * mRate;
-        paymentTowards = fixedMonthly - loan * mRate;
-        loan -= paymentTowards;
+        for (let i = 0; i <= months; i++) {
+            totalInterest += loan * mRate;
+            paymentTowards = fixedMonthly - loan * mRate;
+            loan -= paymentTowards;
+        }
+        return totalInterest;
+    } else {
+        return -1;
     }
-    return totalInterest;
 };
 
 export const getTotalAmountPaid = (loan, totalInterest) => {
-    return Number(loan) + Number(totalInterest);
+    if (isNumber(loan) && isNumber(totalInterest)) {
+        return loan + totalInterest;
+    } else {
+        return -1;
+    }
 };
 
 export const getAmortizedSchedule = (loan, interest, term) => {
-    const fixedMonthly = getMonthlyPayments(loan, interest, term);
-    let interest_rate = 0.0;
-    let paymentTowards = 0.0;
-    let totalInterest = 0.0;
-    let totalPaymentTowardLoan = 0.0;
+    if (isNumber(loan) && isNumber(interest) && isNumber(term)) {
+        const fixedMonthly = getMonthlyPayments(loan, interest, term);
+        let interest_rate = 0.0;
+        let paymentTowards = 0.0;
+        let totalInterest = 0.0;
+        let totalPaymentTowardLoan = 0.0;
 
-    const mRate = interest / (12 * 100);
-    const results = [];
+        const mRate = interest / (12 * 100);
+        const results = [];
 
-    for (let year = 1; year <= term; year++) {
-        for (let month = 1; month <= 12; month++) {
-            interest_rate = loan * mRate;
-            totalInterest += interest_rate;
-            paymentTowards = fixedMonthly - interest_rate;
-            totalPaymentTowardLoan += paymentTowards;
-            loan -= paymentTowards;
+        for (let year = 1; year <= term; year++) {
+            for (let month = 1; month <= 12; month++) {
+                interest_rate = loan * mRate;
+                totalInterest += interest_rate;
+                paymentTowards = fixedMonthly - interest_rate;
+                totalPaymentTowardLoan += paymentTowards;
+                loan -= paymentTowards;
 
-            // add result to array of results so we can print the later
-            results.push({
-                year: year,
-                month: month,
-                interest: interest_rate,
-                principal: paymentTowards,
-                balance: loan,
-                totalInterest: totalInterest,
-                totalPrincipal: totalPaymentTowardLoan,
-            });
+                // add result to array of results so we can print the later
+                results.push({
+                    year: year,
+                    month: month,
+                    interest: interest_rate,
+                    principal: paymentTowards,
+                    balance: loan,
+                    totalInterest: totalInterest,
+                    totalPrincipal: totalPaymentTowardLoan,
+                });
+            }
         }
+        return results;
+    } else {
+        return [];
     }
-    return results;
 };
--- test/calculations.test.js
+++ test/calculations.test.js
@@ -6,7 +6,7 @@ const {
 } = require("../public/scripts/calculations");
 
 describe("Calulation Test Cases", () => {
-    test("Get Monthly Payment -  Good Data", () => {
+    test("Get Monthly Payment - Good Data", () => {
         const amount = 300000;
         const rate = 5;
         const term = 30;
@@ -24,7 +24,7 @@ describe("Calulation Test Cases", () => {
         expect(actual).toBe(expected);
     });
 
-    test("Get Total Interest", () => {
+    test("Get Total Interest - Good Data", () => {
         const amount = 300000;
         const rate = 5;
         const term = 30;
@@ -34,7 +34,18 @@ describe("Calulation Test Cases", () => {
         expect(actual).toBe(expected);
     });
 
-    test("Get Total Amount Paid", () => {
+    test("Get Total Interest - Bad Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const monthlyPayment = getMonthlyPayments(amount, rate, term);
+        const badAmount = "BAD_DATA";
+        const actual = getTotalInterest(monthlyPayment, badAmount, rate, term);
+        const expected = -1;
+        expect(actual).toBe(expected);
+    });
+
+    test("Get Total Amount Paid - Good Data", () => {
         const amount = 300000;
         const rate = 5;
         const term = 30;
@@ -48,5 +59,62 @@ describe("Calulation Test Cases", () => {
         const actual = getTotalAmountPaid(amount, totalInterest);
         const expected = 579767.3528531087;
         expect(actual).toBe(expected);
+    });
+
+    test("Get Total Amount Paid - Bad Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const monthlyPayment = getMonthlyPayments(amount, rate, term);
+        const totalInterest = getTotalInterest(
+            monthlyPayment,
+            amount,
+            rate,
+            term
+        );
+        const badAmount = "BAD_DATA";
+        const actual = getTotalAmountPaid(badAmount, totalInterest);
+        const expected = -1;
+        expect(actual).toBe(-1);
+    });
+
+    test("Get Amortization Schedule - Good Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const actual = getAmortizedSchedule(amount, rate, term);
+        const expectedLength = 360;
+
+        expect(Array.isArray(actual)).toBe(true);
+        expect(actual.length).toBe(expectedLength);
+        const actualFirst = actual[0];
+        const expectedFirst = {
+            year: 1,
+            month: 1,
+            interest: 1250,
+            principal: 360.46486903641926,
+            balance: 299639.5351309636,
+            totalInterest: 1250,
+            totalPrincipal: 360.46486903641926,
+        };
+
+        expect(actualFirst.year).toBe(expectedFirst.year);
+        expect(actualFirst.month).toBe(expectedFirst.month);
+        expect(actualFirst.interest).toBe(expectedFirst.interest);
+        expect(actualFirst.principal).toBe(expectedFirst.principal);
+        expect(actualFirst.balance).toBe(expectedFirst.balance);
+        expect(actualFirst.totalInterest).toBe(expectedFirst.totalInterest);
+        expect(actualFirst.totalPrincipal).toBe(expectedFirst.totalPrincipal);
+    });
+
+    test("Get Amortization Schedule - Bad Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = "BAD_DATA";
+        const actual = getAmortizedSchedule(amount, rate, term);
+        const expectedLength = 0;
+
+        expect(Array.isArray(actual)).toBe(true);
+        expect(actual.length).toBe(expectedLength);
     });
 });

434316a8bee624c9dcb67a10af9d877bb0ae6d07
ricardocarva <ricardofecarva@gmail.com>
2023-11-16 22:33:38 -0500

    Created test cases for Extra Montly Payment for first and last month. Created function to get the savings due to extra payments

--- test/calculations.test.js
+++ test/calculations.test.js
@@ -3,6 +3,8 @@ const {
     getTotalInterest,
     getTotalAmountPaid,
     getAmortizedSchedule,
+    getAmortizedScheduleExtraMonthly,
+    getSavingsExtraMonthly,
 } = require("../public/scripts/calculations");
 
 describe("Calulation Test Cases", () => {
@@ -124,5 +126,85 @@ describe("Calulation Test Cases", () => {
 
         expect(Array.isArray(actual)).toBe(true);
         expect(actual.length).toBe(expectedLength);
+    });
+
+    test("Get Amortization Schedule Extra Monthly Payment - Good Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const extraMonthly = 300;
+        const actual = getAmortizedScheduleExtraMonthly(amount, rate, term, extraMonthly);
+        const expectedLength = 256;
+
+        expect(Array.isArray(actual)).toBe(true);
+        expect(actual.length).toBe(expectedLength);
+        const actualFirst = actual[0];
+        const expectedFirst = {
+            year: 1,
+            month: 1,
+            interest: 1250,
+            principal: 360.46486903641926,
+            balance: 299339.5351309636,
+            totalInterest: 1250,
+            totalPrincipal: 660.46486903641926,
+        };
+
+        expect(actualFirst.year).toBe(expectedFirst.year);
+        expect(actualFirst.month).toBe(expectedFirst.month);
+        expect(actualFirst.interest).toBe(expectedFirst.interest);
+        expect(actualFirst.principal).toBe(expectedFirst.principal);
+        expect(actualFirst.balance).toBe(expectedFirst.balance);
+        expect(actualFirst.totalInterest).toBe(expectedFirst.totalInterest);
+        expect(actualFirst.totalPrincipal).toBe(expectedFirst.totalPrincipal);
+    });
+
+    test("Get Amortization Schedule Extra Monthly Payment Last - Good Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const extraMonthly = 300;
+        const actual = getAmortizedScheduleExtraMonthly(amount, rate, term, extraMonthly);
+        const expectedLength = 256;
+
+        expect(Array.isArray(actual)).toBe(true);
+        expect(actual.length).toBe(expectedLength);
+        const actualLast = actual[expectedLength - 1];
+        const expectedLast = {
+            year: 22,
+            month: 4,
+            interest: 3.560376035187331,
+            principal: 854.4902484449594,
+            balance: 0,
+            totalInterest: 188026.59222876708,
+            totalPrincipal: 301906.90449300135,
+        };
+
+        expect(actualLast.year).toBe(expectedLast.year);
+        expect(actualLast.month).toBe(expectedLast.month);
+        expect(actualLast.interest).toBe(expectedLast.interest);
+        expect(actualLast.principal).toBe(expectedLast.principal);
+        expect(actualLast.balance).toBe(expectedLast.balance);
+        expect(actualLast.totalInterest).toBe(expectedLast.totalInterest);
+        expect(actualLast.totalPrincipal).toBe(expectedLast.totalPrincipal);
+    });
+
+    test("Get Savings Extra Monthly Payment Last - Good Data", () => {
+        const amount = 300000;
+        const rate = 5;
+        const term = 30;
+        const extraMonthly = 300;
+        const actual = getSavingsExtraMonthly(amount, rate, term, extraMonthly);
+
+        console.log(actual);
+
+        expect(Array.isArray(actual)).toBe(true);
+        const actualLast = actual[0];
+        const expectedLast = {
+            months: 8.75,
+            interest: 91740.76062434158,
+        };
+
+        expect(actualLast.months).toBe(expectedLast.months);
+        expect(actualLast.interest).toBe(expectedLast.interest);
     });
 });

46453f0e5eacb28c385e06956d8be8d370937711
Carlos Martinez <martinez.c1@ufl.edu>
2023-12-04 19:25:51 -0500

    Added Comma for Loan Amount Imput
    Makes readability easier for user at the time to enter the "loan amount" value

--- public/scripts/components/InputForm.js
+++ public/scripts/components/InputForm.js
@@ -25,6 +25,82 @@ const s2ab = (s) => {
     }
     return buf;
 };
+const formatNumber = (n) => {
+    // format number 1000000 to 1,234,567
+    return n.replace(/\D/g, "").replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+const parseCurrency = (input) => {
+    const num = input.replace(/[^0-9.]/g, "");
+    return parseFloat(num);
+};
+
+const formatCurrency = (input, blur) => {
+    // appends $ to value, validates decimal side
+    // and puts cursor back in right position.
+
+    // get input value
+    var input_val = input.value;
+
+    // don't validate empty input
+    if (input_val === "") {
+        return;
+    }
+
+    // original length
+    var original_len = input_val.length;
+
+    // initial caret position
+    var caret_pos = input.selectionStart;
+
+    // check for decimal
+    if (input_val.indexOf(".") >= 0) {
+        // get position of first decimal
+        // this prevents multiple decimals from
+        // being entered
+        var decimal_pos = input_val.indexOf(".");
+
+        // split number by decimal point
+        var left_side = input_val.substring(0, decimal_pos);
+        var right_side = input_val.substring(decimal_pos);
+
+        // add commas to left side of number
+        left_side = formatNumber(left_side);
+
+        // validate right side
+        right_side = formatNumber(right_side);
+
+        // On blur make sure 2 numbers after decimal
+        if (blur === "blur") {
+            right_side += "00";
+        }
+
+        // Limit decimal to only 2 digits
+        right_side = right_side.substring(0, 2);
+
+        // join number by .
+        input_val = "$" + left_side + "." + right_side;
+    } else {
+        // no decimal entered
+        // add commas to number
+        // remove all non-digits
+        input_val = formatNumber(input_val);
+        input_val = "$" + input_val;
+
+        // final formatting
+        if (blur === "blur") {
+            input_val += ".00";
+        }
+    }
+
+    // send updated string to input
+    input.value = input_val;
+
+    // put caret back in the right position
+    var updated_len = input_val.length;
+    caret_pos = updated_len - original_len + caret_pos;
+    input[0].setSelectionRange(caret_pos, caret_pos);
+};
 
 export const InputForm = {
     loadEventListeners: () => {},
@@ -101,10 +177,14 @@ export const InputForm = {
             termsContainer.classList.remove("m-auto");
             termsContainer.classList.add("m-15");
         }
-        const loan = Number(document.getElementById("loanAmount").value);
+        const loan = Number(
+            parseCurrency(document.getElementById("loanAmount").value)
+        );
         const interest = Number(document.getElementById("interestRate").value);
         const term = Number(document.getElementById("loanTerm").value);
-        const monthly = Number(document.getElementById("extraMonthlyPayment").value);
+        const monthly = Number(
+            document.getElementById("extraMonthlyPayment").value
+        );
 
         // cap term at 50 years regardless of what they put
         const cap = term <= 50 ? term : 50;
@@ -113,7 +193,6 @@ export const InputForm = {
 
         //const extra = document.getElementById("extraPayments").value;
         if (loan && interest && term) {
-
             const fixedMonthlyPayment = getMonthlyPayments(
                 loan,
                 interest,
@@ -127,17 +206,26 @@ export const InputForm = {
             );
 
             var extraSaving = [];
-            var interestSaving = 0.00;
-            var timeSaving = 0.00;
+            var interestSaving = 0.0;
+            var timeSaving = 0.0;
 
             if (monthly) {
-                extraSaving = getSavingsExtraMonthly(loan, interest, term, monthly)[0];
-                interestSaving = extraSaving.interest
+                extraSaving = getSavingsExtraMonthly(
+                    loan,
+                    interest,
+                    term,
+                    monthly
+                )[0];
+                interestSaving = extraSaving.interest;
                 timeSaving = extraSaving.years;
                 totalInterest = totalInterest - interestSaving;
             }
-            
-            console.log(`Finished ${timeSaving} years or ${timeSaving*12} months earlier`);
+
+            console.log(
+                `Finished ${timeSaving} years or ${
+                    timeSaving * 12
+                } months earlier`
+            );
 
             const totalAmountPaid = getTotalAmountPaid(loan, totalInterest);
 
@@ -145,7 +233,7 @@ export const InputForm = {
                 "Monthly Payment": fixedMonthlyPayment,
                 "Total Interest": totalInterest,
                 "Total Amount Paid": totalAmountPaid,
-                "Interest Saving": interestSaving
+                "Interest Saving": interestSaving,
             };
             console.log(resObj, "resObject");
 
@@ -199,9 +287,8 @@ export const InputForm = {
             );
             var amortSchedule = [];
 
-                amortSchedule = getAmortizedSchedule(monthly, loan, interest, term);
+            amortSchedule = getAmortizedSchedule(monthly, loan, interest, term);
 
-  
             scheduleParent.appendChild(OutputTable.getElement(amortSchedule));
             const lastPaymentItem = amortSchedule[amortSchedule.length - 1];
 
@@ -237,13 +324,15 @@ export const InputForm = {
         label.textContent = labelText;
 
         const input = document.createElement("input");
-        input.type = inputType;
+        input.type = "text";
         input.name = inputName;
         input.id = inputName;
-        
+        input.pattern = "^$d{1,3}(,d{3})*(.d+)?$";
+        input.setAttribute("data-type", "currency");
         input.onkeyup = debounce((e) => {
             if (inputName == "loanAmount") {
                 amountSet = input.value ? true : false;
+                formatCurrency(input);
             } else if (inputName == "interestRate") {
                 rateSet = input.value ? true : false;
             } else if (inputName == "loanTerm") {
@@ -251,13 +340,12 @@ export const InputForm = {
                 if (input.value > 50) input.value = 50;
                 termSet = input.value ? true : false;
             }
-             else if (inputName == "extraMonthlyPayment") {
-                monthlySet = input.value ? true : false;
-             }
             if (amountSet && rateSet && termSet) {
                 InputForm.submitHandler(e);
             }
         }, 550);
+
+        //input.onblur = () => formatCurrency(input.value, "blur");
 
         if (isRequired) {
             input.required = true;
